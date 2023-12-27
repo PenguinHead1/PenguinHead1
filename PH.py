dab72200e8ef3befbacb9101be0055b72b0257da
Pengyy <137204539+PenguinHead1@users.noreply.github.com>
2023-12-27 23:48:55 +0400

    Create PH.py

---
+++ PH.py
@@ -0,0 +1,24 @@
+import random
+from pyfiglet import Figlet
+
+def generate_password(length=12):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+"
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def create_ascii_art(text, font='slant'):
+    figlet = Figlet(font=font)
+    return figlet.renderText(text)
+
+def main():
+    print("Generating a cool password:")
+    password = generate_password()
+    print(password)
+
+    print("\nCool ASCII Art:")
+    ascii_art_text = "Cool Password"
+    ascii_art = create_ascii_art(ascii_art_text)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    main()
